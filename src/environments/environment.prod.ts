d739a889080cb7c2f53128b6a56a8691ac4ac069
Fadil Mucia <fadil_mucia@hotmail.com>
2020-07-10 21:54:17 +0200

    deleted font awesome

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyACDeR_KPanc6h-8EbYuKHHdg1NCGHGJco',
+    authDomain: 'futlink-app.firebaseapp.com',
+    databaseURL: 'https://futlink-app.firebaseio.com',
+    projectId: 'futlink-app',
+    storageBucket: 'futlink-app.appspot.com',
+    messagingSenderId: '405730454949',
+    appId: '1:405730454949:web:e551b3dafc458128ca8c4c',
+    measurementId: 'G-2X66NZHSP1'
+  }
 };
