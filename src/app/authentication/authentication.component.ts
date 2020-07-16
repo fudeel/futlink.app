9a0f3f9a0a93e9fbb05830d84c1516743b7c8561
Fadil Mucia <fadil_mucia@hotmail.com>
2020-07-16 20:07:13 +0200

    user data management on authentication
    Signed-off-by: fudeel <fadilmucia@gmail.com>

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import {auth} from 'firebase';
+import {auth, User} from 'firebase';
 import {Router} from '@angular/router';
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {Coordinates, FutUser} from "../shared/models/FutUser";
+import {AngularFirestore} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-authentication',
@@ -11,12 +13,20 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 })
 export class AuthenticationComponent implements OnInit {
 
+  user: User;
+  lat: number = 0
+  lon: number = 0
+  isPositionError = false;
+  currentUser = '';
+
   isLoading = false;
 
   constructor(
     public fireAuth: AngularFireAuth,
+    private readonly afs: AngularFirestore,
     private readonly router: Router,
-    private readonly snackBar: MatSnackBar) { }
+    private readonly snackBar: MatSnackBar) {
+  }
 
   ngOnInit(): void {
   }
@@ -26,12 +36,7 @@ export class AuthenticationComponent implements OnInit {
       .then(
         () => {
           this.isLoading = true;
-          this.router.navigate(['private']).then(() => this.isLoading = false)
-            .catch(err => {
-              this.isLoading = false
-              this.openSnackBar(err.toString(), 'Close');
-              console.log('Error on logging in: ', err);
-            })
+          this.getCurrentUserDataAndWriteToSession();
         }
       )
       .catch(err => {
@@ -40,9 +45,98 @@ export class AuthenticationComponent implements OnInit {
   }
 
 
+  loginWithFacebook() {
+    this.fireAuth.signInWithPopup(new auth.FacebookAuthProvider())
+      .then(
+        () => {
+          this.isLoading = true;
+          this.getCurrentUserDataAndWriteToSession();
+        }
+      )
+      .catch(err => {
+        console.log('Error on Authentication: ', err);
+      });
+  }
+
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
       duration: 5000,
+    });
+  }
+
+
+  getCurrentUserDataAndWriteToSession() {
+
+    this.fireAuth.user.subscribe(
+      res => {
+        this.user = res;
+        this.checkIfUserExistOnDb(this.user);
+      }, error => {
+      }, () => {
+        console.log('User subscription complete')
+      }
+    );
+
+  }
+
+
+  checkIfUserExistOnDb(user) {
+    this.afs.collection('users').doc(user.uid).valueChanges().subscribe(res => {
+      if (res) {
+        this.updateUserCoordinatesOnDb(user);
+      } else {
+        this.getCurrentUserAndWriteDataOnDbFirstTimeLogin(user);
+      }
+    });
+  }
+
+
+  getCurrentUserAndWriteDataOnDbFirstTimeLogin(user: User) {
+    const futUser: FutUser = {
+      uuid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      elo: 0,
+      coordinates: {
+        lat: this.lat,
+        lon: this.lon
+      },
+
+    };
+
+    this.afs.collection('users').doc(user.uid).set(futUser).then(() => {
+      this.router.navigate(['private']).then(() => this.isLoading = false)
+        .catch(err => {
+          this.isLoading = false
+          this.openSnackBar(err.toString(), 'Close');
+          console.log('Error on logging in: ', err);
+        })
+    });
+  }
+
+
+  updateUserCoordinatesOnDb(user) {
+    const coordinates: Coordinates = {
+      lat: this.lat,
+      lon: this.lon
+    };
+    this.afs.collection('users').doc(user.uid).update({coordinates}).then(() => {
+      this.router.navigate(['private']).then(() => this.isLoading = false)
+        .catch(err => {
+          this.isLoading = false
+          this.openSnackBar(err.toString(), 'Close');
+          console.log('Error on logging in: ', err);
+        })
+    });
+  }
+
+
+  getCurrentCoordinates() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lat = position.coords.latitude;
+      this.lon = position.coords.longitude;
+    }, err => {
+      this.isPositionError = true;
     });
   }
 }
--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
-import {User} from 'firebase';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {Coordinates, FutUser} from '../shared/models/FutUser';
 import {Observable} from 'rxjs';
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {map, shareReplay} from "rxjs/operators";
@@ -15,14 +13,8 @@ import {map, shareReplay} from "rxjs/operators";
 })
 export class PrivateComponent implements OnInit {
 
-  user: User;
-  lat: number;
-  lon: number;
-  isPositionError = false;
-
   isLoading = false;
   currentPage = '';
-  currentUser = '';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -36,55 +28,6 @@ export class PrivateComponent implements OnInit {
               private readonly router: Router) { }
 
   ngOnInit(): void {
-    this.getCurrentUserDataAndWriteToSession();
-    this.getCurrentCoordinates();
-  }
-
-  getCurrentUserDataAndWriteToSession() {
-
-    this.fireAuth.user.subscribe(
-      res => {
-        this.user = res;
-
-        this.checkIfUserExistOnDb(this.user);
-      }
-    );
-
-  }
-
-  checkIfUserExistOnDb(user) {
-    this.afs.collection('users').doc(user.uid).valueChanges().subscribe(res => {
-      if (res) {
-        this.updateUserCoordinatesOnDb(user);
-
-      } else {
-        this.getCurrentUserAndWriteDataOnDbFirstTimeLogin(user);
-      }
-    });
-  }
-
-  getCurrentUserAndWriteDataOnDbFirstTimeLogin(user: User) {
-    console.log('user: ', user);
-    const futUser: FutUser = {
-      uuid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      elo: 0,
-      coordinates: {
-        lat: this.lat,
-        lon: this.lon
-      },
-
-    };
-    this.afs.collection('users').doc(user.uid).set(futUser);
-  }
-
-  updateUserCoordinatesOnDb(user) {
-    const coordinates: Coordinates = {
-      lat: this.lat,
-      lon: this.lon
-    };
-    this.afs.collection('users').doc(user.uid).update({coordinates});
   }
 
   onLogout() {
@@ -96,13 +39,4 @@ export class PrivateComponent implements OnInit {
       });
   }
 
-
-  getCurrentCoordinates() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.lat =  position.coords.latitude;
-      this.lon = position.coords.longitude;
-    }, err => {
-      this.isPositionError = true;
-    });
-  }
 }
